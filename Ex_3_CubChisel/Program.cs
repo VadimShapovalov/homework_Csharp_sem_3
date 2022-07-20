2170c03b304a4909f489ede2902b8ac76d594964
VadimShapovalov <kidav_21@mail.ru>
2022-07-20 21:14:49 +0300

    Нахождение кубов чисел от 1 до заданного

---
+++ Ex_3_CubChisel/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+void KubChisel (int i, int x)
+    {
+        while (i <= x)
+        {
+            int a = i * i * i;
+            Console.WriteLine(a);
+            i++;
+        }
+    }
+
+Console.WriteLine("Напишите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine("Кубы чисел от 1 до заданного: ");
+KubChisel (i , x);
